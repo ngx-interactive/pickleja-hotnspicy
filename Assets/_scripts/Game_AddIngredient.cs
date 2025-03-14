d2a4e05d98cdcdd58fd0c5ea014bc439e3c3a97d
Rayvayvo <rayvayvo@gmail.com>
2025-03-14 13:45:01 -0500

    added add ingredient script

--- Assets/_scripts/Game_AddIngredient.cs
+++ Assets/_scripts/Game_AddIngredient.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game_AddIngredient : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject _ingredientHolder;
+    [SerializeField] private GameObject _ingredientPlaceholder;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Sprite _bread1;
+    [SerializeField] private Sprite _bread2;
+    [SerializeField] private Sprite _bread3;
+
+
+    public void AddIngredient(string _ingredient)
     {
-        
+        if (_ingredientHolder.transform.childCount < 10)
+        {
+            GameObject _tempGO = Instantiate(_ingredientPlaceholder, _ingredientHolder.transform);
+            Image _tempImg = _tempGO.GetComponent<Image>();
+
+            switch (_ingredient)
+            {
+                case "bread1":
+                    _tempImg.sprite = _bread1;
+                    break;
+                case "bread2":
+                    _tempImg.sprite = _bread2;
+                    break;
+                case "bread3":
+                    _tempImg.sprite = _bread3;
+                    break;
+            }
+
+            _tempImg.SetNativeSize();
+        }
     }
 }
