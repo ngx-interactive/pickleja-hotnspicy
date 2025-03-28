24b3a8cc2fa930b9c32bbe6a8f20c5fe8861a59b
Rayvayvo <rayvayvo@gmail.com>
2025-03-28 13:20:35 -0500

    update: added ingredient database

--- Assets/_scripts/Game_IngredientData.cs
+++ Assets/_scripts/Game_IngredientData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game_IngredientData : MonoBehaviour
 {
@@ -13,5 +14,138 @@ public class Game_IngredientData : MonoBehaviour
         public string _tag3;
     }
 
-    
+    [SerializeField] private Sprite[] _breadSpritesList;
+    [SerializeField] private Sprite[] _toppingSpritesList;
+    [SerializeField] private Sprite[] _sauceSpritesList;
+
+    Dictionary<string, Ingredient> _ingredientDatabase;
+
+    private void Start()
+    {
+        _ingredientDatabase = new Dictionary<string, Ingredient>();
+        SeedDatabase();
+    }
+
+    private void SeedDatabase()
+    {
+        //breads
+        _ingredientDatabase.Add("bagel", 
+            new Ingredient { _type = "bread",
+                _sprite = _breadSpritesList[0],
+                _tag1 = "",
+                _tag2 = "",
+                _tag3 = "" });
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[1],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[2],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+
+        //toppings
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[0],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[0],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[0],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[0],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[0],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[0],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+
+        //sauces
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[0],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[0],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[0],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+        _ingredientDatabase.Add("bagel",
+        new Ingredient
+        {
+            _type = "",
+            _sprite = _breadSpritesList[0],
+            _tag1 = "",
+            _tag2 = "",
+            _tag3 = ""
+        });
+    }
 }
